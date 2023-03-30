a96388c43870404e7f27572da2f36156e02ec420
devesh1011 <devesh.22scse1100004@galgotiasuniversity.edu.in>
2023-03-30 10:59:22 +0530

    Some Small changes and Converted from Class based to Function based component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,36 +1,30 @@
 import React, { Component } from 'react'
 // import PropTypes from 'prop-types'
 
-export class Navbar extends Component {
-    static propTypes = {
-
-    }
-
-    render() {
-        return (
-            <>
-                <nav className="navbar navbar-expand-lg bg-body-tertiary shadow">
-                    <div className="container-fluid">
-                        <a className="navbar-brand" href="/">NewsHub</a>
-                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                            <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul className="navbar-nav mb-2 mb-lg-0 ms-auto">
-                                <li className="nav-item">
-                                    <a rel='noreferrer' className="nav-link active" aria-current="page" href="/">Home</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a rel='noreferrer' className="nav-link" href="/">Link</a>
-                                </li>
-                            </ul>
-                        </div>
+const Navbar = () => {
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary shadow">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="/">NewsHub</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav mb-2 mb-lg-0 ms-auto">
+                            <li className="nav-item">
+                                <a rel='noreferrer' className="nav-link active" aria-current="page" href="/">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a rel='noreferrer' className="nav-link" href="/">Link</a>
+                            </li>
+                        </ul>
                     </div>
-                </nav>
-            </>
-        )
+                </div>
+            </nav>
+        </>
+    )
     }
-}
 
 export default Navbar
 
